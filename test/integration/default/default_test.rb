a6b672a047906b9725c5b17b13e55a5062bdd525
Nick James <nick.james@eaglegenomics.com>
2020-04-08 15:18:51 +0100

    renaming integration test dir name

---
+++ test/integration/default/default_test.rb
@@ -0,0 +1,25 @@
+# # encoding: utf-8
+
+# Inspec test for recipe MUSiCC::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+# Check the run MUSiCC script is installed
+describe command('which run_musicc.py') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match %r{/run_musicc.py} }
+end
+
+# Check that MUSiCC test script is installed
+describe command('which test_musicc.py') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match %r{/test_musicc.py} }
+end
+
+# Run the MUSiCC test script
+describe command('test_musicc.py') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match(/Running time was/) }
+  its('stderr') { should match(/OK/) }
+end
